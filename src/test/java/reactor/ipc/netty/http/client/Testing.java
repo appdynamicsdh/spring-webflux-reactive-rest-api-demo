7cfb5e0236b7000a410aea6b54ba8ea9c186759c
David Hope (dahope) <dahope@cisco.com>
2020-01-10 09:49:52 -0500

    first commit

---
+++ src/test/java/reactor/ipc/netty/http/client/Testing.java
@@ -0,0 +1,17 @@
+package reactor.ipc.netty.http.client;
+
+public class Testing {
+
+    public void test(){
+
+
+        org.springframework.web.server.ServerWebExchange p = null;
+        p.getRequest().getHeaders().get("");
+        p.getRequest().getPath();
+
+        org.springframework.web.reactive.DispatcherHandler c;
+
+        org.springframework.web.reactive.function.server.support.HandlerFunctionAdapter j;
+
+    }
+}
